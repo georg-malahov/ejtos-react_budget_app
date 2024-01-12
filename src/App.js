323def41898b223417eb0c5cda842cfd262a1f91
Georg Malahov <georg@malahov.io>
2024-01-12 17:39:46 +0100

    finish set up

--- src/App.js
+++ src/App.js
@@ -9,36 +9,34 @@ import Budget from './components/Budget';
 
 
 import { AppProvider } from './context/AppContext';
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import ExpenseItem from './components/ExpenseItem';
+import AllocationForm from './components/AllocationForm';
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
-                        {
-                            /* Add Budget component here */
-                        }        
+                        <div className='col-sm'>
+                            <Budget />
+                        </div>
 
-                        {
-                            /* Add Remaining component here*/
-                        }        
+                        <div className='col-sm'>
+                            <Remaining />
+                        </div>    
 
-                        {
-                            /* Add ExpenseTotal component here */
-                        }        
+                        <div className='col-sm'>
+                            <ExpenseTotal />
+                        </div>      
                        
-                        {
-                            /* Add ExpenseList component here */
-                        }         
-
-                        {
-                            /* Add ExpenseItem component here */
-                        }        
-
-                        {
-                            /* Add AllocationForm component here under */
-                        }        
-
+                        <ExpenseList />
+                
+                        <ExpenseItem />
+                    
+                        <AllocationForm />
                 </div>
             </div>
         </AppProvider>
